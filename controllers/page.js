359c8ccd00aafcbe2da41dbe4c34a7a39eec90c9
youngrong.hwang <dudfhd!13>
2024-10-23 15:59:11 +0900

    nunjucks view file commit

--- controllers/page.js
+++ controllers/page.js
@@ -1,10 +1,10 @@
-exports.renderProfile = (req, res, next) => {
+exports.renderProfileController = (req, res, next) => {
     // res.locals와 아래 {title} 부분 같이 view 파일로 넘어감
     res.render('profile', { title: '내 정보 - X' }); // profile.html
 };
-exports.renderJoin = (req, res, next) => {
+exports.renderJoinController = (req, res, next) => {
     res.render('join', { title: '회원 가입 - X' }); // join.html
 };
-exports.renderMain = (req, res, next) => {
+exports.renderMainController = (req, res, next) => {
     res.render('main', { title: 'X', twits: [], }); // main.html
 };
--- routes/page.js
+++ routes/page.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const { renderJoin, renderMain, renderProfile } = require('../controllers/page');
+const { renderJoinController, renderProfileController, renderMainController} = require('../controllers/page');
 
 router.use((req, res, next) => {
    // locals(지역변수)에 저장시 다른 라우터 들에서 다 접근이 가능함 : 미들웨어, 뷰간 데이터 전달이 가능함
@@ -14,8 +14,8 @@ router.use((req, res, next) => {
 });
 
 // controller로 분리
-router.get('/profile', renderProfile);
-router.get('/join', renderJoin);
-router.get('/', renderMain);
+router.get('/profile', renderProfileController);
+router.get('/join', renderJoinController);
+router.get('/', renderMainController);
 
 module.exports = router;
